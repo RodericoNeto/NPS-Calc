ab0a4d1ea33294fff01d06374bcc6ca0c23cbe3f
RodericoNeto <rodericocrneto@gmail.com>
2022-07-23 20:31:10 -0300

    Calculadora funcionando só com variaveis simples

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 
-
-let corpoNotas = {
-    detratores: 0,
-    neutros: 0,
-    promotores: 0
-};
+let detratores = 0;
+let neutros = 0;
+let promotores = 0;
 
 
 function submitNota() {
     var notaDadaJS = document.querySelectorAll('input[type=radio]:checked')[0].value;
     alert(`A nota escolhida foi: ${notaDadaJS}. \n Obrigado pela participação!`);
     console.log(notaDadaJS);
-    
+
 
     switch (notaDadaJS) {
         case "0":
@@ -21,21 +18,18 @@ function submitNota() {
         case "4":
         case "5":
         case "6":
-            corpoNotas.detratores++;
-            console.log(corpoNotas);
-            resetRadio();      
+            detratores++;
+            resetRadio();
             break;
         case "7":
         case "8":
-            corpoNotas.neutros++;
-            console.log(corpoNotas);
-            resetRadio(); 
+            neutros++;
+            resetRadio();
             break;
         case "9":
         case "10":
-            corpoNotas.promotores++;
-            console.log(corpoNotas);
-            resetRadio(); 
+            promotores++;
+            resetRadio();
             break;
         default:
             alert("Por favor, insira uma nota")
@@ -44,20 +38,28 @@ function submitNota() {
 
 
 function calcularNPS() {
-    var totalRespostas = corpoNotas.detratores + corpoNotas.neutros + corpoNotas.promotores;
+    var totalRespostas = detratores + neutros + promotores;
     alert(`Total de respostas: ${totalRespostas}
-            \n detratores: ${corpoNotas.detratores} 
-            \n neutros: ${corpoNotas.neutros}
-            \n promotores: ${corpoNotas.promotores}`);
-    var resultNPS = ((corpoNotas.promotores - corpoNotas.detratores)/totalRespostas)*100;
+            \n detratores: ${detratores} 
+            \n neutros: ${neutros}
+            \n promotores: ${promotores}`);
+    var resultNPS = ((promotores - detratores) / totalRespostas) * 100;
+    document.getElementById("respostaNPS").innerHTML = `O valor do nps foi ${resultNPS.toFixed(2)}%`
     alert(`O valor do nps foi ${resultNPS.toFixed(2)}%`)
+}
+
+function resetPesquisa(){
+    document.getElementById("respostaNPS").innerHTML = ``;
+    detratores = 0;
+    neutros = 0;
+    promotores = 0;
 }
 
 function resetRadio() {
     var radioButton = document.getElementsByName("notas");
-    for(var i=0; i<radioButton.length; i++){
+    for (var i = 0; i < radioButton.length; i++) {
         radioButton[i].checked = false;
-    }   
+    }
 }
 
 
