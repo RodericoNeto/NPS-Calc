79d7c8ac4f588d20be4060df3ae7f94842c078b2
RodericoNeto <rodericocrneto@gmail.com>
2022-07-24 15:44:59 -0300

    nps com interface mais amigavel mas cores estranhas ainda

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 let detratores = 0;
 let neutros = 0;
 let promotores = 0;
+let totalNotas = 0;
 
 
 function submitNota() {
     var notaDadaJS = document.querySelectorAll('input[type=radio]:checked')[0].value;
+    
     alert(`A nota escolhida foi: ${notaDadaJS}. \n Obrigado pela participação!`);
     console.log(notaDadaJS);
-
 
     switch (notaDadaJS) {
         case "0":
@@ -34,25 +35,35 @@ function submitNota() {
         default:
             alert("Por favor, insira uma nota")
     }
+    totalNotas++;
+    document.getElementById("totalRespostas").innerHTML = `Total de respostas submetidas: ${totalNotas}`;
 }
 
 
 function calcularNPS() {
-    var totalRespostas = detratores + neutros + promotores;
-    alert(`Total de respostas: ${totalRespostas}
+    alert(`Total de respostas: ${totalNotas}
             \n detratores: ${detratores} 
             \n neutros: ${neutros}
             \n promotores: ${promotores}`);
-    var resultNPS = ((promotores - detratores) / totalRespostas) * 100;
-    document.getElementById("respostaNPS").innerHTML = `O valor do nps foi ${resultNPS.toFixed(2)}%`
+    var resultNPS = ((promotores - detratores) / totalNotas) * 100;
+    document.getElementById("respostaNPS").innerHTML = `O valor do nps foi ${resultNPS.toFixed(2)}%`;
+    calcBar()
     alert(`O valor do nps foi ${resultNPS.toFixed(2)}%`)
 }
 
+function calcBar() {
+    var progressBarJS = document.querySelector(".progress-bar");
+    progressBarJS.setAttribute(`id`, `play-animation`);
+}
+
 function resetPesquisa(){
+    document.getElementById("totalRespostas").innerHTML = ``;
     document.getElementById("respostaNPS").innerHTML = ``;
+    resetRadio();
     detratores = 0;
     neutros = 0;
     promotores = 0;
+    totalNotas = 0;
 }
 
 function resetRadio() {
